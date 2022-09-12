39bf509de5401e2dda4cade25b3643596fdeeb6a
lxcxcxlxmb <lxcxcxlxmbx@gmail.com>
2022-09-12 13:35:16 -0300

    .

--- .history/api/routes/index_20220901150321.js
+++
@@ -1,9 +0,0 @@
-const cors = require('cors');
-const router = require('express').Router();
-const users = require('./users');
-
-router.use(cors());
-
-router.use(users);
-
-module.exports = router;
